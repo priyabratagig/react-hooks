0ca477b4b867c10450986ee1469b8204f3b76dc3
priyabratagig <priyabratachattaraj23@gmail.com>
2022-05-06 15:29:54 +0530

    class setState second argument immediately invoked function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import StateCounterClass from './Components/useState/StateCounterClass'
 import StateCounterFunctional from './Components/useState/StateCounterFunctional';
+// only call hook within react functional components at the top level
+// don't call hooks inside loops, condition, or nested functions
 function App() {
   return (
     <div className="App">
--- src/Components/useState/StateCounterClass.js
+++ src/Components/useState/StateCounterClass.js
@@ -7,11 +7,18 @@ export class StateCounterClass extends Component {
             ids: [],
         };
     }
+    addItems = function () {
+        this.setState({
+            ids: [...this.state.ids, { id: this.state.ids.length, value: Math.floor(Math.random() * 10) + 1 }]
+        },
+            () => { alert('Added ' + JSON.stringify(this.state.ids[this.state.ids.length - 1])); } //immediately invoked after evry setState call
+        );
+    }
     render() {
         return (
             <div>
                 {/* need to copy the array and assign updates */}
-                <button onClick={() => this.setState({ ids: [...this.state.ids, { id: this.state.ids.length, value: Math.floor(Math.random() * 10) + 1 }] })}>Add Item</button>
+                <button onClick={() => this.addItems()}>Add Item</button>
                 {this.state.ids.map(ele => (<p key={ele.id}>{ele.id} - {ele.value}</p>))}
             </div>
         )
