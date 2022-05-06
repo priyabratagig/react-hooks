8182cc5caa68e8431dd945ef74b8760f7e2d8944
priyabratagig <priyabratachattaraj23@gmail.com>
2022-05-07 00:53:08 +0530

    data fetching with useEffect

--- src/Components/useEffect/EffectCounterFunctional.js
+++ src/Components/useEffect/EffectCounterFunctional.js
@@ -1,28 +1,49 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react'
 
 function EffectCounterFunctional() {
-    const [count, setCount] = useState(0);
-    // const updateCount = () => {
-    //     setCount(count + 1);
-    //     // setCount(count => count + 1); solution-2
-    // };
+    const [posts, setPosts] = useState([]);
+    const [post, setPost] = useState({ id: 1 });
+    const [postId, setPostId] = useState(1);
     useEffect(() => {
-        const updateCount = () => {
-            setCount(count + 1);
-            // whenever need to call a function inside useEffect define it inside useEffect, that way don't forget to keep track of props and states dependency
-        };
-        const interval = setInterval(updateCount, 1000);
-        return () => {
-            clearInterval(interval);
-        }
-    }, [count]); //run only once
-    // useEff with empty dependency
-    // will run once and don't watch changes any veriable
-    // at re-render count is changed and upCount's count rfferencing old count
-    // Dependency array is not to when re-run the effect, instade let react know everything that effect must watch for changes
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                setPosts(response.data);
+            }).catch(({ message }) => {
+                setPosts([{ id: 'Error', title: message }]);
+            });
+    }, []);
+    useEffect(() => {
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${post.id}`)
+            .then(response => setPost(response.data))
+            .catch(({ message }) => setPost({
+                id: post.id,
+                title: 'Error',
+                body: message,
+            }))
+    }, [post.id]);
     return (
         <div>
-            <p>EffectCounterFunctional Count - {count}</p>
+            <p>EffectCounterFunctional</p>
+            <input type="number" value={postId} onChange={({ target: { value } }) => { setPostId(value) }} />
+            <button onClick={() => setPost({ id: postId })}>Fetch Post</button>
+            <p>Post ID {post.id}</p>
+            <p>Post Title: {post.title}</p>
+            <p>Post Body: {post.body}</p>
+            <table style={{ margin: '0 auto' }}>
+                <thead>
+                    <tr>
+                        <td>Post-ID</td>
+                        <td>Post-Title</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {posts.map(post => (<tr key={post.id}>
+                        <td>{post.id}</td>
+                        <td>{post.title}</td>
+                    </tr>))}
+                </tbody>
+            </table>
         </div>
     )
 }
