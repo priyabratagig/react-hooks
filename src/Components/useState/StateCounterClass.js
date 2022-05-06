42f59380726fc461bd74b6589e9c6e64a6267073
priyabratagig <priyabratachattaraj23@gmail.com>
2022-05-06 15:05:50 +0530

    setState update array (spread) Math.random

--- src/Components/useState/StateCounterClass.js
+++ src/Components/useState/StateCounterClass.js
@@ -11,7 +11,7 @@ export class StateCounterClass extends Component {
         return (
             <div>
                 {/* need to copy the array and assign updates */}
-                <button onClick={() => this.setState({ ids: [...this.state.ids, { id: this.state.ids.length, value: Math.floor(Math.random(10) + 1) }] })}>Add Item</button>
+                <button onClick={() => this.setState({ ids: [...this.state.ids, { id: this.state.ids.length, value: Math.floor(Math.random() * 10) + 1 }] })}>Add Item</button>
                 {this.state.ids.map(ele => (<p key={ele.id}>{ele.id} - {ele.value}</p>))}
             </div>
         )
--- src/Components/useState/StateCounterFunctional.js
+++ src/Components/useState/StateCounterFunctional.js
@@ -5,7 +5,7 @@ function StateCounterFunctional() {
     return (
         <div>
             {/* need to copy the array and assign updates */}
-            <button onClick={() => setState([...state, { id: state.length, value: Math.floor(Math.random(10) + 1) }])}>Add Item</button>
+            <button onClick={() => setState([...state, { id: state.length, value: Math.floor(Math.random() * 10) + 1 }])}>Add Item</button>
             {state.map(ele => (<p key={ele.id}>{ele.id} - {ele.value}</p>))}
         </div>
     )
