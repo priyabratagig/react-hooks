4fca079b41066216394c39b7be91ae85297f5477
priyabratagig <priyabratachattaraj23@gmail.com>
2022-05-06 21:59:45 +0530

    previousProps, previousState, dependency array

--- src/Components/useEffect/EffectCounterClass.js
+++ src/Components/useEffect/EffectCounterClass.js
@@ -5,18 +5,23 @@ export class EffectCounterClass extends Component {
         super(props);
         this.state = {
             count: 0,
+            name: '',
         };
     }
     componentDidMount() {
         document.title = `Class Clicked ${this.state.count}` //same code writen twice
     };
-    componentDidUpdate() {
-        document.title = `Class Clicked ${this.state.count}` //same code writen twice
+    componentDidUpdate(previousProps, previousState) {
+        if (previousState.count !== this.state.count) { //run side effect with condition
+            console.log('Document title updated')
+            document.title = `Class Clicked ${this.state.count}` //same code writen twice
+        }
     }
     render() {
         return (
             <div>
                 <p>EffectCounterClass</p>
+                <input type="name" value={this.state.name} onChange={({ target: { value } }) => this.setState({ ...this.state, name: value })} />
                 <button onClick={() => this.setState({ count: this.state.count + 1 })}>Cliked {this.state.count}</button>
             </div>
         )
--- src/Components/useEffect/EffectCounterFunctional.js
+++ src/Components/useEffect/EffectCounterFunctional.js
@@ -2,12 +2,15 @@ import React, { useState, useEffect } from 'react'
 
 function EffectCounterFunctional() {
     const [counter, setCounter] = useState(0);
+    const [name, setName] = useState('');
     useEffect(() => {
+        console.log('Document title updated')
         document.title = `Function Clicked ${counter}`
-    })
+    }, [counter]); //run effect with condition
     return (
         <div>
             <p>EffectCounterFunctional</p>
+            <input type="name" value={name} onChange={({ target: { value } }) => setName(value)} />
             <button onClick={() => setCounter(counter + 1)}>Clicked {counter}</button>
         </div>
     )
