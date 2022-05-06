ad1d454f4f4a9072cfe3525cacd4cd79e3e648a6
priyabratagig <priyabratachattaraj23@gmail.com>
2022-05-06 14:37:55 +0530

    setState function as argument

--- src/Components/useState/StateCounterClass.js
+++ src/Components/useState/StateCounterClass.js
@@ -10,12 +10,15 @@ export class StateCounterClass extends Component {
     }
     incrementCounter() {
         this.setState({ counter: this.state.counter + 1 }); //always take object or function that returns an object
-    }
+    } incrementCounterFive = () => [
+        this.setState(state => ({ counter: state.counter + 1 })) //will pass previous state, react execute changes in batch
+    ]
     render() {
         return (
             <div>
-                <p>StateCounterClass</p>
-                <button onClick={this.incrementCounter}>Count {this.state.counter}</button>
+                <p>StateCounterClass - Count - {this.state.counter}</p>
+                <button onClick={this.incrementCounter}>Increment 1</button>
+                <button onClick={() => { for (let i of [1, 2, 3, 4, 5]) this.incrementCounterFive() }}>Increment 5</button>
             </div>
         )
     }
--- src/Components/useState/StateCounterFunctional.js
+++ src/Components/useState/StateCounterFunctional.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 
 function StateCounterFunctional() {
     const [counter, setCounter] = useState(0);
+    const incrementCounterFive = () => setCounter(counter => counter + 1)
     return (
         <div>
-            <p>StateCounterFunctional</p>
-            <button onClick={() => setCounter(counter + 1)}>Count {counter}</button>
+            <p>StateCounterFunctional Counter - {counter}</p>
+            <button onClick={() => setCounter(counter + 1)}>Increment 1</button>
+            <button onClick={() => { for (let i of [1, 2, 3, 4, 5]) incrementCounterFive() }}>Increment 5</button>
         </div>
     )
 }
